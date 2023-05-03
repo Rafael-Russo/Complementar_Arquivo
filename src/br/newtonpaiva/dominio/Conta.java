8206d2a1f3d76ad5a95ae0ed076d30cd8827cdd9
12118482 <11800585@aluno.cotemig.com.br>
2023-05-02 21:31:52 -0300

    feat: inicial

---
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -0,0 +1,31 @@
+package br.newtonpaiva.dominio;
+
+public class Conta {
+    private String agencia;
+    private String numero;
+    private Double saldo;
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
